d477bf9a5d2ac4de1fb64a598c89a4bf31759ee3
John Rees <john@bitsushi.com>
2015-05-16 01:47:02 +0100

    graph

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,8 @@
 class StationsController < ApplicationController
+  layout false
+  def show
+    @station = Station.find(params[:id])
+
+    @readings = @station.recordings.map{|r| {x: r.polled_at.to_i, y: r.bikes_count } }
+  end
 end
