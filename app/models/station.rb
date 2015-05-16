73438161c79e5fa186c6a488dd4d7de6a30a89b9
John Rees <john@bitsushi.com>
2015-05-16 02:17:15 +0100

    step graphs

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,12 @@
 require 'cgi'
 class Station < ActiveRecord::Base
   has_many :recordings
+
+  def total_slots
+    bikes_count + slots_count
+  end
+
+  def to_s
+    [street_name, street_number].compact.join(', ')
+  end
 end
